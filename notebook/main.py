21bc8c7dd28ed698baecaedf9e76616f382d98a6
txdas <bjjinyalong@corp.netease.com>
2024-12-25 23:52:39 +0800

    add stock fund

--- notebook/main.py
+++ notebook/main.py
@@ -4,5 +4,5 @@ from sklearn.preprocessing import  KBinsDiscretizer
 model = LinearSVC()
 model.fit
 # ak.stock_zh_a_spot_em()
-# ak.stock_a_indicator_lg
+ak.stock_individual_fund_flow
 print(ak.stock_main_fund_flow().sort_values("今日排行榜-今日排名")[["代码","名称","今日排行榜-今日排名"]][:25])
